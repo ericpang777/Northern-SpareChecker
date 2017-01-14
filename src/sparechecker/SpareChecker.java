56d31dbaea4079a25868144bd0b8c7fbe254c033
ericpang777 <eric.pang@live.com>
2017-01-13 22:56:02 -0500

    Iniitial Commit

---
+++ src/sparechecker/SpareChecker.java
@@ -0,0 +1,10 @@
+package sparechecker;
+
+public class SpareChecker {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/sparechecker/Student.java
@@ -0,0 +1,37 @@
+package sparechecker;
+
+public class Student {
+	private int day;
+	private int period;
+	private String firstName;
+	private String lastName;
+	private int studentNumber;
+	
+	public Student(int day, int period, String firstName, String lastName, int studentNumber) {
+		this.day = day;
+		this.period = period;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.studentNumber = studentNumber;
+	}
+
+	public int getDay() {
+		return day;
+	}
+
+	public int getPeriod() {
+		return period;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public int getStudentNumber() {
+		return studentNumber;
+	}
+}
